9c45a71b1292c5ebcb590d276b4b519e389fe1b9
chaos-gorilla <184151260+chaos-gorilla@users.noreply.github.com>
2026-09-08 09:47:04 +0530

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        /**
+            State Tracking
+            -> rows: stores the characters seen row wise.
+            -> cols: stores the set of characters seen in a given col.
+            -> squares: normalised storage of the characters seen in each blocks.
+        */
+        unordered_map<int, unordered_set<char>> rows;
+        unordered_map<int, unordered_set<char>> cols;
+        map<const pair<int, int>, unordered_set<char>> square_blocks;
+
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                char ch = board[row][col];
+
+                if (ch == '.') {
+                    continue;
+                }
+
+                // 1. Check the rows and cols, if they contain duplicate characters in the same line.
+                if (rows[row].find(ch) != rows[row].end() or cols[col].find(ch) != cols[col].end()) {
+                    return false;
+                }
+                
+                const pair<int, int> sudoku_block_index = {row / 3, col / 3};
+                // 2. Check the square brackets, and see if there are duplicate elements in the same block.
+                if (square_blocks[sudoku_block_index].find(ch) != square_blocks[sudoku_block_index].end()) {
+                    return false;
+                }
+
+                // 3. Mark it as a valid move, and then update all the sets we are maintaining.
+                square_blocks[sudoku_block_index].insert(ch);
+                rows[row].insert(ch);
+                cols[col].insert(ch);
+            }
+        }
+
+        return true;
+    }
+};
